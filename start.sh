e6f1d0ad09a19a86bb9d4a803c28c0aeb02ef4b6
Elvin Taghizade <elvintaghiyev184@gmail.com>
2026-07-19 00:38:49 +0300

    single point of run, start.sh initialized

---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+echo "[1/4] [START] Force-killing any Zombie containers..."
+docker rm -f voqe-service || true
+echo "[1/4] [FINISH] Force-killed any Zombie containers."
+
+echo "[2/4] [START] Cleaning up networks and volumes..."
+docker compose -f configs/infra/docker-compose.yml down --remove-orphans
+echo "[2/4] [FINISHED] Cleaned up networks and volumes."
+
+echo "[3/4] [START] Building project with Gradle..."
+./gradlew clean build test jacocoTestReport -x javadoc
+echo "[3/4] [FINISHED] Built project with Gradle."
+
+echo "[4/4] [START] Initializing Docker Infrastructure..."
+docker compose -f configs/infra/docker-compose.yml up -d --build
+echo "[4/4] [FINISHED] Initialized Docker Infrastructure."
+
+echo "[5/5] [START] Tailing logs (Press Ctrl+C to stop watching)..."
+docker compose -f configs/infra/docker-compose.yml logs -f
