88ff3228ac56dba0d10c5646a4767ce633c1a3be
Elvin Taghizade <elvintaghiyev184@gmail.com>
2026-07-23 22:31:48 +0300

    docker compose path updated for start.sh and stop.sh

--- infra/init-scripts/start.sh
+++ infra/init-scripts/start.sh
@@ -7,7 +7,7 @@ docker rm -f voqe-service || true
 echo "[1/4] [FINISH] Force-killed any Zombie containers."
 
 echo "[2/4] [START] Cleaning up networks and volumes..."
-docker compose -f infra/docker-compose.yml down --remove-orphans
+docker compose -f ../docker-compose.yml down --remove-orphans
 echo "[2/4] [FINISHED] Cleaned up networks and volumes."
 
 echo "[3/4] [START] Building project with Gradle..."
@@ -15,8 +15,8 @@ echo "[3/4] [START] Building project with Gradle..."
 echo "[3/4] [FINISHED] Built project with Gradle."
 
 echo "[4/4] [START] Initializing Docker Infrastructure..."
-docker compose -f infra/docker-compose.yml up -d --build
+docker compose -f ../docker-compose.yml up -d --build
 echo "[4/4] [FINISHED] Initialized Docker Infrastructure."
 
 echo "[5/5] [START] Tailing logs (Press Ctrl+C to stop watching)..."
-docker compose -f infra/docker-compose.yml logs -f
+docker compose -f ../docker-compose.yml logs -f
--- infra/init-scripts/stop.sh
+++ infra/init-scripts/stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Stopping Docker Infrastructure..."
-docker compose -f configs/infra/docker-compose.yml down --remove-orphans -v
+docker compose -f ../docker-compose.yml down --remove-orphans -v
 echo "Environment shutdown complete!"
